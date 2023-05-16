ac8ea2b53f9e2568e8ab0b8cb8d921ca1d1e56b5
Romain Ruetschi <romain@informal.systems>
2023-05-16 15:11:12 +0200

    Use GPT-4

--- plugins/chatgpt.lua
+++ plugins/chatgpt.lua
@@ -1,7 +1,14 @@
 return {
   "jackMort/ChatGPT.nvim",
   config = function()
-    require("chatgpt").setup()
+    require("chatgpt").setup({
+      openai_params = {
+        model = "gpt-4"
+      },
+      openai_edit_params = {
+        mode = "gpt-4"
+      }
+    })
   end,
   cmd = {
     "ChatGPT",
