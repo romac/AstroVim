f532cf3e75e06f754385f21429f55c26dbcc28b1
Romain Ruetschi <106849+romac@users.noreply.github.com>
2023-08-08 17:55:22 +0200

    Enable formatting for Nix and Swift

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -2,7 +2,7 @@ return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -11,6 +11,9 @@ return {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettier,
+
+      null_ls.builtins.formatting.alejandra,
+      null_ls.builtins.formatting.swiftformat,
     }
     return config -- return final config table
   end,
