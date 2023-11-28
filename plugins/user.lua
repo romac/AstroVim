ffd6e321a0c1d43f09f46b6f8e20ad2195b215c6
Romain Ruetschi <github@romac.me>
2023-11-28 09:33:03 +0100

    Add `todo-comments`

--- plugins/user.lua
+++ plugins/user.lua
@@ -6,5 +6,21 @@ return {
   {
     "rhysd/vim-llvm",
     enabled = true,
-  }
+  },
+  {
+    "folke/todo-comments.nvim",
+    enabled = true,
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = { "TodoTelescope" },
+    opts = {},
+    init = function()
+      vim.api.nvim_create_user_command("TodoEnable", function() require("todo-comments").enable() end, { nargs = 0 })
+      vim.api.nvim_create_user_command("TodoDisable", function() require("todo-comments").disable() end, { nargs = 0 })
+      vim.api.nvim_create_user_command(
+        "Todo",
+        function() require("todo-comments.search").setloclist() end,
+        { nargs = 0 }
+      )
+    end,
+  },
 }
