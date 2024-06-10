2e042ec1ab3aa6ed94a7b2d18868a938fa6d7013
Romain Ruetschi <106849+romac@users.noreply.github.com>
2024-06-10 12:36:42 +0200

    Disable buggy formatter for Fish

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -18,19 +18,17 @@ return {
       -- control auto formatting on save
       format_on_save = {
         enabled = true, -- enable or disable format on save globally
-        allow_filetypes = { -- enable format on save for specified filetypes only
-          -- "go",
-        },
+        allow_filetypes = {}, -- enable format on save for specified filetypes only
         ignore_filetypes = { -- disable format on save for specified filetypes
           "crates.nvim",
           "markdown",
           "python",
           "toml",
+          "fish",
         },
       },
       disabled = { -- disable formatting capabilities for the listed language servers
-        -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
-        -- "lua_ls",
+        "fish",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
