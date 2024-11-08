4bd1c64c40680f0c1117a85ee42de3caade8d247
Romain Ruetschi <github@romac.me>
2024-11-08 21:26:27 +0100

    Add instant-rename

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -65,6 +65,12 @@ return {
   },
 
   {
+    "mei28/instant_rename.nvim",
+    event = { "ModeChanged", "CmdlineChanged" }, -- for lazy loading
+    config = function() require "instant_rename" end,
+  },
+
+  {
     "florentc/vim-tla",
     ft = "tla",
   },
