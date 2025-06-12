622c251a77e69df6e246403310f7d9ed5e07e672
Romain Ruetschi <github@romac.me>
2025-06-12 15:50:17 +0200

    Update Mason config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,27 +2,28 @@
 
 ---@type LazySpec
 return {
-  -- use mason-tool-installer for automatically installing Mason packages
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     -- overrides `require("mason-tool-installer").setup(...)`
     opts = {
       -- Make sure to use the names found in `:Mason`
       ensure_installed = {
-        -- install language servers
+        -- Lua
         "lua-language-server",
-        "rust-analyzer",
-        "haskell-language-server",
-
-        -- install formatters
-        -- "alejandra",
-        "prettier",
         "stylua",
 
-        -- install debuggers
-        -- "debugpy",
+        -- Rust
+        "rust-analyzer",
 
-        -- install any other package
+        -- Haskell
+        "haskell-language-server",
+        "fourmolu",
+
+        -- install formatters
+        -- JavaScript/TypeScript
+        "prettier",
+
+        -- Other
         "tree-sitter-cli",
         "typos-lsp",
       },
