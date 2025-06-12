7fbc671919ec9a2a588bf9aa31dafd03cb18d607
Romain Ruetschi <github@romac.me>
2025-06-12 15:49:51 +0200

    Misc

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -22,11 +22,13 @@ return {
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
     diagnostics = {
       virtual_text = {
+        -- current_line = true,
         severity = {
           max = vim.diagnostic.severity.WARN,
         },
       },
       virtual_lines = {
+        -- current_line = true,
         severity = {
           min = vim.diagnostic.severity.ERROR,
         },
