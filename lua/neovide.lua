ca662bc8f403a5d523acc8fe887ef3248dc5bc94
Romain Ruetschi <github@romac.me>
2025-12-29 12:21:48 +0100

    Neovide config

---
+++ lua/neovide.lua
@@ -0,0 +1,24 @@
+if not vim.g.neovide then return end
+
+vim.opt.linespace = 5
+
+vim.o.guifont = "JetBrainsMonoNL Nerd Font Mono:h14"
+vim.g.neovide_opacity = 1.0
+vim.g.neovide_window_blurred = false
+vim.g.neovide_show_border = false
+
+-- No animations
+vim.g.neovide_position_animation_length = 0
+vim.g.neovide_cursor_animation_length = 0.00
+vim.g.neovide_cursor_trail_size = 0
+vim.g.neovide_cursor_animate_in_insert_mode = false
+vim.g.neovide_cursor_animate_command_line = false
+vim.g.neovide_scroll_animation_far_lines = 0
+vim.g.neovide_scroll_animation_length = 0.00
+
+vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
